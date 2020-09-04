f9d8de7be9fd73f721d0043361b6b95e3d790ee6
Sharon Lin <hsiang.lin@gmail.com>
2020-09-04 14:53:41 -0400

    Add react components, Index file in client react folder error message need to be resoolved

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import axios from 'axios'
-
+import { Navbar, Nav } from "react-bootstrap";
+import Products from './components/Products'
+import CustomProducts from './components/CustomProducts'
+import Stores from './components/Stores'
+import Categories from './components/Categories'
+import Variations from './components/Variations'
+import { LinkContainer } from 'react-router-bootstrap';
 
 class App extends Component {
   state = {users: []}
@@ -16,15 +22,61 @@ class App extends Component {
 
   render() {
     return (
-    <div>
-      {this.state.users.map(user => (
-        <li>
-          {user.name} = {user.username}
-        </li>
-      ))}
-    </div>
+    <Router>
+      <Navbar bg = "info" variant = "dark">
+        <LinkContainer to="/">
+          <Navbar.Brand>Tee Commerce Database</Navbar.Brand>
+        </LinkContainer>
+        <Nav className = "mr-auto">
+          <LinkContainer to= "/products">
+            <Nav.Link>Products</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to = "/stores">
+            <Nav.Link>Stores</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to = "/categories">
+            <Nav.Link>Categories</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to = "/variations">
+            <Nav.Link>Variations</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to = "/customProducts">
+            <Nav.Link>CustomProducts</Nav.Link>
+          </LinkContainer>
+        </Nav>
+        </Navbar>
+        
+        <Switch>
+          <Route exact path = "/">
+            <Home />
+          </Route>
+          <Route path = "/products">
+            <Products />
+          </Route>
+          <Route path = "/stores">
+            <Stores />
+          </Route>
+          <Route path = "/variations">
+            <Variations/>
+          </Route>
+          <Route path = "/customProducts">
+            <CustomProducts/>
+          </Route>
+          <Route path = "/categories">
+            <Categories/>
+          </Route>
+        </Switch>
+      </Router>
     )
   }
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
 }
 
 export default App;
